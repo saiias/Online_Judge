367350e64aa2bb620ecabffeecd5aac57a7ad083
yuuki <sa.nevermind@gmail.com>
2011-10-09 16:33:52 +0900

    add 2619

--- 2140.cpp
+++ 2140.cpp
@@ -15,6 +15,7 @@ using namespace std;
 
 int main(){
   int n=0;
+
   scanf("%d",&n);
   long long ans=1;
   for(int i = 2; i*i<n*2;i++){
---
+++ 2894.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+#include<algorithm>
+#include<stack>
+#include<queue>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<complex>
+
+#define DEBUG(x) cout<<"line"<<__LINE__<<":"<<#x" == "<<x<<endl
+#define REP(i,n) for(int i = 0; i < (int)(n); i++)
+#define INF 1000000
+using namespace std;
+///////////////////////////////////////////////////////////////
+int main()
+{
+  int n;
+  cin>>n;
+  for(int i = 0;i<n;i++){
+    int m;
+    cin>>m;
+    vector<int>aa,bb;
+    
+    for(int j = 0;j<m;m++){
+    char c;
+    cin>>c>>aa[j]>>bb[j];
+    }
+    
+
+    
+  }
+  
+  return 0;
+  
+}
+
