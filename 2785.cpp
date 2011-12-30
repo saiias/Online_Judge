4dafe16216003e49865051f908ee4482a4bede82
sai <sa.nevermind@gmail.com>
2011-12-30 17:03:40 +0900

    submit problems

---
+++ 2785.cpp
@@ -0,0 +1,50 @@
+﻿#include<iostream>
+#include<stack>
+#include<vector>
+#include<algorithm>
+#include<stack>
+#include<queue>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<cmath>
+#include<complex>
+#include<sstream>
+#include<map>
+#include<set>
+#define DEBUG(x) cout<<"line"<<__LINE__<<":"<<#x" == "<<x<<endl
+#define REP(i,n) for(int i = 0; i < (int)(n); i++)
+#define ALL(x) (x).begin(),(x).end()
+#define INF 1000000
+using namespace std;
+
+inline int toInt(string s) {int v; istringstream sin(s);sin>>v;return v;}
+template<class T> inline string toString(T x) {ostringstream sout;sout<<x;return sout.str();}
+typedef long long ll;
+typedef pair<int,int> P;
+
+#define MAX_N 4000
+///////////////////////////////////////////////////////////////
+int A[MAX_N],B[MAX_N],C[MAX_N],D[MAX_N];
+int CD[MAX_N*MAX_N];
+int main(){
+    int n;
+    cin>>n;
+    REP(i,n){
+        cin>>A[i]>>B[i]>>C[i]>>D[i];
+    }
+    REP(i,n)REP(j,n){
+        CD[i*n+j]=C[i]+D[j];
+    }
+    sort(CD,CD+n*n);
+    ll res = 0;
+
+    REP(i,n)REP(j,n){
+        int cd = -A[i]-B[j];
+        res+=upper_bound(CD,CD+n*n,cd)-lower_bound(CD,CD+n*n,cd);
+}
+    cout<<res<<endl;
+    return 0;
+  
+}
+
--- 3684.cpp
+++ 3684.cpp
@@ -17,18 +17,39 @@ using namespace std;
 ///////////////////////////////////////////////////////////////
 int N,H,R,T;
 double y[MAX_N];
+const double g = 10.0;
 
 double cals(int T)
 {
   if(T<0) return H;
-  return 0;
+  double t = sqrt(2*H/g);
+  int k = (int)(T/t);
+  double d;
+  if(k%2 ==0){
+      d = T-k*t;
+      return H-g*d*d/2;
+
+  }else{
+      d =k*t+t-T;
+      return H-g*d*d/2;
+
+  }
+        
 }
 
 int main()
 {
+    int C;
+    cin>>C;
+    while(C--){
+        memset(y,0.0,sizeof(y));
+        cin>>N>>H>>R>>T;
+        REP(i,N) y[i] =cals(T-i);
 
-  
-  return 0;
+        sort(y,y+N);
+        REP(i,N) printf("%.2f%c",y[i]+2*R*i/100.0,i+1 == N?'\n':' ');
+    }
+    return 0;
   
 }
 
