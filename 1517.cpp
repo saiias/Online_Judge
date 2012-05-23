fbca25ac22e9496bbc726603c3577dbaf3178be4
yuuki <sa.nevermind@gmail.com>
2012-05-23 10:45:06 +0900

    accept 1517

--- 1517.cpp
+++ 1517.cpp
@@ -16,9 +16,6 @@
 #define REP(i,n) for(int i = 0; i < (int)(n); i++)
 #define ALL(x) (x).begin(),(x).end()
 #define INF 1000000
-#define EPS 1e-9
-#define PI 3.14159
-
 using namespace std;
 
 inline int toInt(string s) {int v; istringstream sin(s);sin>>v;return v;}
@@ -26,22 +23,25 @@ template<class T> inline string toString(T x) {ostringstream sout;sout<<x;return
 typedef long long ll;
 typedef pair<int,int> P;
 
+
 ///////////////////////////////////////////////////////////////
 int main(){
-  string in;
-  while(cin>>in){
-    if(in=="ENDOFINPUT") break;
-    int x=0,y=0,z=0;
-    cin>>x>>y>>z;
-    z = min(z,360-z);
-    
-    if(2*x*PI*(z/180.0)<y*5+EPS){
-      cout<<"YES "<<(int)(y-(2*x*(PI*z/180))/5+EPS)<<endl;
-    }else{
-      cout<<"NO "<<y*5<<endl;
-    }
-    cin>>in;
+  cout<<"n e"<<endl;
+  cout<<"_ ___________"<<endl;
+  double a =1.0;
+  double b = 0.0;
+  REP(i,10){
+    int temp;
+    if(i==0) temp = 1;
+    else temp *=i;
+
+    a=1.0/temp;
+    b+=a;
+    printf("%d %.10g\n",i,b);
   }
-  return 0;
+  
+  
+    return 0;
+  
 }
 
